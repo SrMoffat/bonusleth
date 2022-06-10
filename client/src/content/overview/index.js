35f7b3bb1e0fa369c3194e401cb61938d708a979
SrMoffat <ngigemoffat@gmail.com>
2022-06-10 15:27:30 +0300

    feat(hoome): add home age

--- client/src/content/overview/index.js
+++ client/src/content/overview/index.js
@@ -1,25 +1,58 @@
 import { styled } from '@mui/material/styles';
 
-import { Box } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+import { Container, Grid, Typography, Button } from '@mui/material';
+import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
 
-const GalaxyPaper = styled('div')(
-  () => `
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-image: url(/static/images/placeholders/covers/galaxy.png);
-`
-);
+import PageTitleWrapper from 'src/components/PageTitleWrapper';
+import Footer from 'src/components/Footer';
 
 function Overview() {
+  const user = {
+    name: 'Catherine Pike',
+    avatar: '/static/images/avatars/1.jpg'
+  };
   return (
-    <Box>Home page</Box>
-    // <GalaxyPaper>
-    //   <div style={{ border: '1px solid red' }}>NNavBard</div>
-    //   <div style={{ border: '1px solid red' }}>World Map</div>
-    // </GalaxyPaper>
+    <>
+      <Helmet>
+        <title>Transactions</title>
+      </Helmet>
+      <PageTitleWrapper>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <Typography variant="h3" component="h3" gutterBottom>
+              Home
+            </Typography>
+            <Typography variant="subtitle2">
+              {user.name}, your have <span>30 Stacos</span> to give away
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              sx={{ mt: { xs: 2, md: 0 } }}
+              variant="contained"
+              startIcon={<AddTwoToneIcon fontSize="small" />}
+            >
+              Create transaction
+            </Button>
+          </Grid>
+        </Grid>
+      </PageTitleWrapper>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12}>
+            Here
+          </Grid>
+        </Grid>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
